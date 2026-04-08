a0643577fe5d63af668882d5e51be26a40ffdf00
suhailkhadas <khadassuhail@gmail.com>
2026-04-08 10:30:29 +0530

    task 13

--- JS/SuhailKhadas-task13/scripts.js
+++ JS/SuhailKhadas-task13/scripts.js
@@ -1,2 +1,13 @@
-let a=1,b=2,c=3,d=4;
+let number = 101;
 
+let even= "is even";
+let odd= "is odd";
+
+let result;
+
+if(number % 2=== 0){
+    result= console.log("The number " + number+ " " + even);
+}
+else{
+     result= console.log("The number " + number +" "+ odd);
+}

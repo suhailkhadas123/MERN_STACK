20ba600635ff2af35559e8e7bb9d3f7f261a5a4f
suhailkhadas <khadassuhail@gmail.com>
2026-04-25 14:39:21 +0530

    task 20

--- JS/SuhailKhadas-task20/scripts.js
+++ JS/SuhailKhadas-task20/scripts.js
@@ -30,19 +30,59 @@ function update() {
 }
 
 function book(){
-     event.preventDefault();
-     const message= document.getElementById("book-section")
-    if(cart.length ==0){
-        
-        message.innerHTML="<p>Please Add Service/Services in cart to Book</p>"
-       alert("Please Add Service/Services in cart to Book")
+     const message= document.getElementById("book-section");
+     const name = document.getElementById("input-name").value;
+     const email = document.getElementById("input-email").value;
+     const phone = document.getElementById("input-number").value;
+     const total = document.getElementById("total-amount").textContent;
+
+    if(cart.length == 0){
+        message.innerHTML="<p>Please Add Service/Services in cart to Book</p>";
+        alert("Please Add Service/Services in cart to Book");
     }
     else{
+      if(name.length == 0 || email.length == 0 || phone.length == 0){
+        message.innerHTML="<p>Enter Details</p>";
+        alert("Enter Details");
+      }else{
+        
         message.innerHTML="<p>Waiting For Confirmation</p>";
-        setTimeout(function booking(){
-            
-            message.innerHTML="<p>Booking Successfull</p>";
-            alert("Booking Successfull");
-        }, 3000)
+
+        
+
+        // sendBookingEmail(name, email, phone, cart, total);
+      }
     }
 }
+
+
+
+        (function() {
+           
+            emailjs.init({
+              publicKey: "yW7xU8iVc0tC15p9k",
+            });
+        })();
+   window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // these IDs from the previous steps
+                emailjs.sendForm('service_2oufhxf', 'template_yl5h0bv', this)
+                    .then(() => {
+                       setTimeout(function booking(){
+          ;
+            alert("Thank you For Booking the Service We will get back to you soon!");
+        }, 3000);
+                    }, (error) => {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
+
+
+
+
+
+
+
+
